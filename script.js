58b70e540dec8b56b464c1c27bcbd3c453ae1066
kevinpa1000 <kevinkantonap@gmail.com>
2023-05-29 18:32:56 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var buttonContainer = document.getElementById("buttonContainer");
+
+for (var i = 1; i <= 20; i++) {
+    var button = document.createElement("button");
+    button.textContent = i;
+    button.addEventListener("click", function() {
+        alert(this.textContent);
+    });
+    buttonContainer.appendChild(button);
+}
